582b2bc8207b87d23a01863564857ef7ad5a0f34
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2025-10-04 19:09:13 +0200

    Update site title and favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `The Kruuk`,
+  description: `The Kruuk | Official Website | All our upcoming shows.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
