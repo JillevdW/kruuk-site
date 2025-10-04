fefe62096271f8e0895e0c298c132149e311ab49
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2025-10-04 18:38:18 +0200

    Display band photo in footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,10 +5,11 @@ import { imageURL } from "@/lib/imageurl";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200 dark:border-slate-600 dark:bg-slate-800">
+    <footer className="bg-neutral-50 border-t border-neutral-200 dark:border-slate-600 dark:bg-slate-800 bg-[url(/kruuk-site/assets/images/banner-min.jpg)] bg-cover bg-center">
       <Container>
-        <div className="flex content-center justify-center">
+        <div className="flex content-center justify-center pt-48 pb-8">
           <div className="flex-grow"></div>
+          <div className="flex content-center justify-center bg-slate-800 bg-opacity-70 rounded-lg">
           <Link href="https://open.spotify.com/artist/5WtVI5nBs62jIhw7OQxfxr" className="m-4 flex items-center">
             <Image src={imageURL("/assets/images/socials/spotify.svg")} width={40} height={40} alt="Spotify icon" className="block dark:hidden"></Image>
             <Image src={imageURL("/assets/images/socials/spotify_light.svg")} width={40} height={40} alt="Spotify icon" className="hidden dark:block"></Image>
@@ -28,6 +29,7 @@ export function Footer() {
             <Image src={imageURL("/assets/images/socials/applemusic.svg")} width={40} height={40} alt="Spotify icon" className="block dark:hidden"></Image>
             <Image src={imageURL("/assets/images/socials/applemusic_light.svg")} width={40} height={40} alt="Spotify icon" className="hidden dark:block"></Image>
           </Link>
+          </div>
           <div className="flex-grow"></div>
         </div>
         {/* <div className="py-28 flex flex-col lg:flex-row items-center">
