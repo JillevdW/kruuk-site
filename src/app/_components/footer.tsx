b65ae777fd675ef4da31ec63a399d722788c24b5
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2025-10-04 15:42:37 +0200

    Add social media images to footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,11 +1,32 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { imageURL } from "@/lib/imageurl";
 
 export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="flex content-center justify-center">
+          <div className="flex-grow"></div>
+          <Link href="https://open.spotify.com/artist/5WtVI5nBs62jIhw7OQxfxr" className="m-4 flex items-center">
+            <Image src={imageURL("/assets/images/socials/spotify.svg")} width={40} height={40} alt="Spotify icon"></Image>
+          </Link>
+
+          <Link href="https://www.instagram.com/thekruukband/" className="m-4 flex items-center">
+            <Image src={imageURL("/assets/images/socials/instagram.svg")} width={40} height={40} alt="Spotify icon"></Image>
+          </Link>
+
+          <Link href="https://www.youtube.com/@thekruukband" className="m-4 flex items-center">
+            <Image src={imageURL("/assets/images/socials/youtube.svg")} width={40} height={40} alt="Spotify icon"></Image>
+          </Link>
+
+          <Link href="https://music.apple.com/nl/artist/the-kruuk/1756516525" className="m-4 flex items-center">
+            <Image src={imageURL("/assets/images/socials/applemusic.svg")} width={40} height={40} alt="Spotify icon"></Image>
+          </Link>
+          <div className="flex-grow"></div>
+        </div>
+        {/* <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Statically Generated with Next.js.
           </h3>
@@ -23,7 +44,7 @@ export function Footer() {
               View on GitHub
             </a>
           </div>
-        </div>
+        </div> */}
       </Container>
     </footer>
   );
