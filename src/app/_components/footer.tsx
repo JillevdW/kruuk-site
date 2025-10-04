591f5026f35747940bfd5a74de0810ed25ce2fda
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2025-10-04 17:06:18 +0200

    Add dark mode images for the footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,24 +5,28 @@ import { imageURL } from "@/lib/imageurl";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
+    <footer className="bg-neutral-50 border-t border-neutral-200 dark:border-slate-600 dark:bg-slate-800">
       <Container>
         <div className="flex content-center justify-center">
           <div className="flex-grow"></div>
           <Link href="https://open.spotify.com/artist/5WtVI5nBs62jIhw7OQxfxr" className="m-4 flex items-center">
-            <Image src={imageURL("/assets/images/socials/spotify.svg")} width={40} height={40} alt="Spotify icon"></Image>
+            <Image src={imageURL("/assets/images/socials/spotify.svg")} width={40} height={40} alt="Spotify icon" className="block dark:hidden"></Image>
+            <Image src={imageURL("/assets/images/socials/spotify_light.svg")} width={40} height={40} alt="Spotify icon" className="hidden dark:block"></Image>
           </Link>
 
           <Link href="https://www.instagram.com/thekruukband/" className="m-4 flex items-center">
-            <Image src={imageURL("/assets/images/socials/instagram.svg")} width={40} height={40} alt="Spotify icon"></Image>
+            <Image src={imageURL("/assets/images/socials/instagram.svg")} width={40} height={40} alt="Spotify icon" className="block dark:hidden"></Image>
+            <Image src={imageURL("/assets/images/socials/instagram_light.svg")} width={40} height={40} alt="Spotify icon" className="hidden dark:block"></Image>
           </Link>
 
           <Link href="https://www.youtube.com/@thekruukband" className="m-4 flex items-center">
-            <Image src={imageURL("/assets/images/socials/youtube.svg")} width={40} height={40} alt="Spotify icon"></Image>
+            <Image src={imageURL("/assets/images/socials/youtube.svg")} width={40} height={40} alt="Spotify icon" className="block dark:hidden"></Image>
+            <Image src={imageURL("/assets/images/socials/youtube_light.svg")} width={40} height={40} alt="Spotify icon" className="hidden dark:block"></Image>
           </Link>
 
           <Link href="https://music.apple.com/nl/artist/the-kruuk/1756516525" className="m-4 flex items-center">
-            <Image src={imageURL("/assets/images/socials/applemusic.svg")} width={40} height={40} alt="Spotify icon"></Image>
+            <Image src={imageURL("/assets/images/socials/applemusic.svg")} width={40} height={40} alt="Spotify icon" className="block dark:hidden"></Image>
+            <Image src={imageURL("/assets/images/socials/applemusic_light.svg")} width={40} height={40} alt="Spotify icon" className="hidden dark:block"></Image>
           </Link>
           <div className="flex-grow"></div>
         </div>
