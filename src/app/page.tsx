d4652f8cb00528337af2d39a52dcec802a70e172
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2025-10-04 15:22:36 +0200

    No longer attempts to load posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,20 @@ import { GigList } from "@/app/components/giglist";
 import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
-  const allPosts = getAllPosts();
+  // const allPosts = getAllPosts();
 
-  const heroPost = allPosts[0];
+  // const heroPost = allPosts[0];
 
-  const morePosts = allPosts.slice(1);
+  // const morePosts = allPosts.slice(1);
 
   return (
     <main>
       <Container>
         <Intro />
 
-        <GigList />
-
-        {/* <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
-
+        <div className="mb-32">
+          <GigList />
+        </div>
 
       </Container>
     </main>
