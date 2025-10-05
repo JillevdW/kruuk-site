f025646be0efc5203d2d1651e98466b5f4c66fab
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2025-10-05 14:25:41 +0200

    Adds a link to the homepage to the logo

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,6 +1,7 @@
 import { CMS_NAME } from "@/lib/constants";
 import { imageURL } from "@/lib/imageurl";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Intro() {
   return (
@@ -8,12 +9,14 @@ export function Intro() {
       {/* <h1 className="text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         The Kruuk
       </h1> */}
-      <Image src={imageURL("/assets/images/kruuk_roze.png")} height={70} width={200} alt="Kruuk logo"
-        className="block dark:hidden"
-      ></Image>
-      <Image src={imageURL("/assets/images/kruuk_light.png")} height={50} width={150} alt="Kruuk logo"
-        className="hidden dark:block my-8"
-      ></Image>
+      <Link href="/">
+        <Image src={imageURL("/assets/images/kruuk_roze.png")} height={70} width={200} alt="Kruuk logo"
+          className="block dark:hidden"
+        ></Image>
+        <Image src={imageURL("/assets/images/kruuk_light.png")} height={50} width={150} alt="Kruuk logo"
+          className="hidden dark:block my-8"
+        ></Image>
+      </Link>
     </section>
   );
 }
