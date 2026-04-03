2d02f18a3c0cd0cfe4f228ddeecbf56ba5936055
Jille van der Weerd <23138608+JillevdW@users.noreply.github.com>
2026-04-03 12:41:29 +0200

    Commit all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
 import { GigList } from "@/app/components/giglist";
-import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
-  // const allPosts = getAllPosts();
-
-  // const heroPost = allPosts[0];
-
-  // const morePosts = allPosts.slice(1);
-
   return (
     <main>
       <Container>
@@ -20,7 +11,6 @@ export default function Index() {
         <div className="mb-32">
           <GigList />
         </div>
-
       </Container>
     </main>
   );
